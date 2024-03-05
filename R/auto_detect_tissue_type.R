ba5ce8214b16fa09a10365ea5adbc046cdcdd9c1
Kristen Michelle Nader <61286548+kris-nader@users.noreply.github.com>
2024-03-05 09:48:10 +0200

    Update auto_detect_tissue_type.R

--- R/auto_detect_tissue_type.R
+++ R/auto_detect_tissue_type.R
@@ -8,45 +8,54 @@
 # @params: seuratObject - The Seurat Object from wich to extract the input scRNA-seq matrix (rownames - genes, column names - cells), 
 # @params: scale - indicates whether the matrix is scaled (TRUE by default)
 # @params: assay - e.g. RNA, SCT, integrated
-
 auto_detect_tissue_type <- function(path_to_db_file, seuratObject, scaled, assay = "RNA", ...){
-  
-  # get all tissue types in DB
+    
+    # get all tissue types in DB
     db_read = openxlsx::read.xlsx(path_to_db_file); tissues_ = unique(db_read$tissueType); result_ = c()
     
     for(tissue in tissues_){ print(paste0("Checking...", tissue));
         
-    # prepare gene sets
-    gs_list = gene_sets_prepare(path_to_db_file, tissue);
-    
-    # prepare obj
-    if(scaled){
-      obj = as.matrix(seuratObject[[assay]]@scale.data)
-    } else {
-      obj = as.matrix(seuratObject[[assay]]@counts)
+        # prepare gene sets
+        gs_list = gene_sets_prepare(path_to_db_file, tissue);
+        
+        package_type=substr(packageVersion("Seurat"), 1, 1)
+        if (package_type==5){
+            if(scaled){
+                obj = as.matrix(seuratObject[[assay]]$scale.data)
+            } else {
+                obj = as.matrix(seuratObject[[assay]]$counts)
+            }
+        }
+        else{
+            if(scaled){
+                obj = as.matrix(seuratObject[[assay]]@scale.data)
+            } else {
+                obj = as.matrix(seuratObject[[assay]]@counts)
+            }
+        }
+        
+        
+        es.max = sctype_score(scRNAseqData = obj, scaled = scaled, gs = gs_list$gs_positive, gs2 = gs_list$gs_negative, 
+                              marker_sensitivity = gs_list$marker_sensitivity, verbose=!0);
+        
+        cL_resutls = do.call("rbind", lapply(unique(seuratObject@meta.data$seurat_clusters), function(cl){
+            
+            es.max.cl = sort(rowSums(es.max[ ,rownames(seuratObject@meta.data[seuratObject@meta.data$seurat_clusters==cl, ])]), decreasing = !0)
+            head(data.frame(cluster = cl, type = names(es.max.cl), scores = es.max.cl), 10)
+        }))
+        
+        dt_out = cL_resutls %>% group_by(cluster) %>% top_n(n = 1)
+        
+        # return mean score for tissue
+        result_ = rbind(result_, data.frame(tissue = tissue, score = mean(dt_out$scores)))
     }
-                            
-    es.max = sctype_score(scRNAseqData = obj, scaled = scaled, gs = gs_list$gs_positive, gs2 = gs_list$gs_negative, 
-                          marker_sensitivity = gs_list$marker_sensitivity, verbose=!0);
-  
-    cL_resutls = do.call("rbind", lapply(unique(seuratObject@meta.data$seurat_clusters), function(cl){
-      
-        es.max.cl = sort(rowSums(es.max[ ,rownames(seuratObject@meta.data[seuratObject@meta.data$seurat_clusters==cl, ])]), decreasing = !0)
-        head(data.frame(cluster = cl, type = names(es.max.cl), scores = es.max.cl), 10)
-    }))
-                          
-    dt_out = cL_resutls %>% group_by(cluster) %>% top_n(n = 1)
     
-    # return mean score for tissue
-    result_ = rbind(result_, data.frame(tissue = tissue, score = mean(dt_out$scores)))
-  }
-  
-  # order by mean score
-  result_ = result_[order(-result_$score),]
-  
-  # plot 
-  barplot(height=result_$score, names=result_$tissue, col=rgb(0.8,0.1,0.1,0.6),
-          xlab="Tissue", ylab="Summary score",  main="The higher summary score, the more likely tissue type is")
-  
-  result_
+    # order by mean score
+    result_ = result_[order(-result_$score),]
+    
+    # plot 
+    barplot(height=result_$score, names=result_$tissue, col=rgb(0.8,0.1,0.1,0.6),
+            xlab="Tissue", ylab="Summary score",  main="The higher summary score, the more likely tissue type is")
+    
+    result_
 }
